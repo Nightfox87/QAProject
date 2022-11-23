9774944e7ebc967fbc4342e5d2df3d019e8c289e
Дина Абрамова <nightfox87@mail.ru>
2022-11-23 20:42:26 +0300

    Tests added

--- src/test/java/ru/netology/tour/page/CardPage.java
+++ src/test/java/ru/netology/tour/page/CardPage.java
@@ -20,8 +20,16 @@ public class CardPage {
     private SelenideElement cvcCodeField = $("[placeholder='999']");
     private SelenideElement continueButton = $(byText("Продолжить"));
     private SelenideElement successNotification = $("[class*=notification_status_ok]");
+    private SelenideElement errorNotification = $("[class*=notification_status_error]");
     private SelenideElement incorrectMonthNote = $(byText("Неверно указан срок действия карты"));
     private SelenideElement expiredYearNote = $(byText("Истёк срок действия карты"));
+    private SelenideElement wrongFormatCardNumber = $(byXpath("//*[@id=\"root\"]/div/form/fieldset/div[1]/span/span/span[3]"));
+    private SelenideElement wrongFormatMonth = $(byXpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span[1]/span/span/span[3]"));
+    private SelenideElement wrongFormatYear = $(byXpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span[2]/span/span/span[3]"));
+    private SelenideElement wrongFormatCvcCode = $(byXpath("//*[@id=\"root\"]/div/form/fieldset/div[3]/span/span[2]/span/span/span[3]"));
+    private SelenideElement emptyNameField = $(byText("Поле обязательно для заполнения"));
+
+
 
     public CardPage payForTheTour(DataGenerator.CardInfo info) {
         buyButton.click();
@@ -35,6 +43,7 @@ public class CardPage {
     public void shouldAppearSuccessNotification() {
         successNotification.shouldBe(visible, Duration.ofSeconds(15));
     }
+    public void shouldAppearErrorNotification() { errorNotification.shouldBe(visible, Duration.ofSeconds(15)); }
     public void shouldClickContinueButton() {
         continueButton.click();
     }
@@ -45,7 +54,7 @@ public class CardPage {
     public void shouldSetMonth(int month) {
         monthField.setValue(String.valueOf(month));
     }
-    public void shouldAppearIncorrectMonthNotification(){
+    public void incorrectMonthNotification(){
         incorrectMonthNote.shouldBe(visible);
     }
     public void clearYearField() {
@@ -55,8 +64,33 @@ public class CardPage {
     public void shouldSetYear(int year) {
         yearField.setValue(String.valueOf(year));
     }
-    public void shouldAppearExpiredYearNotification() {
+    public void clearCvcField() {
+        cvcCodeField.doubleClick();
+        cvcCodeField.sendKeys(Keys.BACK_SPACE);
+    }
+    public void shouldSetCVC(String code) {cvcCodeField.setValue(code);}
+    public void clearCardNumberField() {
+        cardNumberField.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        cardNumberField.sendKeys(Keys.BACK_SPACE);
+    }
+    public void shouldSetCardNumber(String cardNumber) {
+        cardNumberField.setValue(cardNumber);
+    }
+    public void clearNameField() {
+        nameField.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        nameField.sendKeys(Keys.BACK_SPACE);
+    }
+    public void shouldSetNameField(String name) {
+        nameField.setValue(name);
+    }
+    public void expiredYearNotification() {
         expiredYearNote.shouldBe(visible);
     }
+    public void shouldClickBuyButton() {buyButton.click();}
+    public void wrongFormatNotificationCardNumber() {wrongFormatCardNumber.shouldBe(visible);}
+    public void wrongFormatNotificationMonth() {wrongFormatMonth.shouldBe(visible);}
+    public void wrongFormatNotificationYear() {wrongFormatYear.shouldBe(visible);}
+    public void wrongFormatNotificationCvc() { wrongFormatCvcCode.shouldBe(visible);}
+    public void emptyNameFieldNotification() {emptyNameField.shouldBe(visible);}
 
 }
--- src/test/java/ru/netology/tour/test/CardTest.java
+++ src/test/java/ru/netology/tour/test/CardTest.java
@@ -14,6 +14,7 @@ public class CardTest {
     void setup() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:8080");
+        var cardPage = new CardPage();
     }
 
     @Test
@@ -24,12 +25,29 @@ public class CardTest {
         cardPage.shouldClickContinueButton();
         cardPage.shouldAppearSuccessNotification();
     }
-
     /*@Test
+    void shouldGetInfoFromDatabase() {}*/
+
+    @Test
+    void shouldNotBuyTourWithDeclinedCard() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getIncorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.shouldClickContinueButton();
+        cardPage.shouldAppearErrorNotification();
+    }
+
+    @Test
     void shouldNotSendEmptyForm() {
-
-    }*/
-
+        var cardPage = new CardPage();
+        cardPage.shouldClickBuyButton();
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationCardNumber();
+        cardPage.wrongFormatNotificationMonth();
+        cardPage.wrongFormatNotificationYear();
+        cardPage.emptyNameFieldNotification();
+        cardPage.wrongFormatNotificationCvc();
+    }
     @Test
     void shouldNotSendFormWithIncorrectMonth() {
         var cardPage = new CardPage();
@@ -38,7 +56,7 @@ public class CardTest {
         cardPage.clearMonthField();
         cardPage.shouldSetMonth(17);
         cardPage.shouldClickContinueButton();
-        cardPage.shouldAppearIncorrectMonthNotification();
+        cardPage.incorrectMonthNotification();
     }
     @Test
     void shouldNotSendFormWithExpiredYear() {
@@ -48,7 +66,115 @@ public class CardTest {
         cardPage.clearYearField();
         cardPage.shouldSetYear(21);
         cardPage.shouldClickContinueButton();
-        cardPage.shouldAppearExpiredYearNotification();
+        cardPage.expiredYearNotification();
+    }
+    @Test
+    void shouldNotSendFormWithOneDigitMonth() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearMonthField();
+        cardPage.shouldSetMonth(9);
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationMonth();
+    }
+    @Test
+    void shouldNotSendFormWithOneDigitYear() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearYearField();
+        cardPage.shouldSetYear(9);
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationYear();
+    }
+    @Test
+    void shouldNotSendFormWithSixPlusYear() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearYearField();
+        cardPage.shouldSetYear(30);
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationYear();
+    }
+    @Test
+    void shouldNotSendFormWith3ZeroCVC() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearCvcField();
+        cardPage.shouldSetCVC("000");
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationCvc();
+    }
+    @Test
+    void shouldNotSendFormWithOneDigitCVC() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearCvcField();
+        cardPage.shouldSetCVC("1");
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationCvc();
+    }
+    @Test
+    void shouldNotSendFormWithOneDigitCardNumber() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearCardNumberField();
+        cardPage.shouldSetCardNumber("1");
+        cardPage.shouldClickContinueButton();
+        cardPage.wrongFormatNotificationCardNumber();
+    }
+    @Test
+    void shouldNotBuyTourWithAnotherCard() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearCardNumberField();
+        cardPage.shouldSetCardNumber("2222 2222 2222 2222");
+        cardPage.shouldClickContinueButton();
+        cardPage.shouldAppearErrorNotification();
+    }
+    @Test
+    void shouldNotSendFormWithRussianName() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("ru");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.shouldClickContinueButton();
+        cardPage.shouldAppearErrorNotification();
+    }
+    @Test
+    void shouldNotSendFormWithDigitsInName() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearNameField();
+        cardPage.shouldSetNameField("12345678");
+        cardPage.shouldClickContinueButton();
+        cardPage.shouldAppearErrorNotification();
+    }
+    @Test
+    void shouldNotSendFormWithSymbolsInName() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearNameField();
+        cardPage.shouldSetNameField("@#$%^&");
+        cardPage.shouldClickContinueButton();
+        cardPage.shouldAppearErrorNotification();
+    }
+    @Test
+    void shouldNotSendFormWithOneLetterInName() {
+        var cardPage = new CardPage();
+        var cardInfo = DataGenerator.getCorrectCardInfo("en");
+        cardPage.payForTheTour(cardInfo);
+        cardPage.clearNameField();
+        cardPage.shouldSetNameField("N");
+        cardPage.shouldClickContinueButton();
+        cardPage.shouldAppearErrorNotification();
     }
 
 
